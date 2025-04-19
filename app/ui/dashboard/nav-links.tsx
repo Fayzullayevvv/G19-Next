d144c9a90fa80ffc3ebf75ef4ae1d89a96bdb2b7
Fayzullayevvv <tell2537505@gmail.com>
2025-04-19 16:54:46 +0500

    init

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,12 @@
+"use client"  
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -17,6 +20,14 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname  = usePathname();
+  console.log(pathname);
+
+
+
+
+
+
   return (
     <>
       {links.map((link) => {
